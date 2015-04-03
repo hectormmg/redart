6a53d8f51d2a56b160807e64c5e2aa7e9af61fb4
hector <hectormgwake@gmail.com>
2015-04-03 16:03:04 -0600

    Make landing page with animated background

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home_page#landing'
+
+  get 'home_page/signup'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
